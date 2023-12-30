55a29b230faf7efaf302ec5edcf93c5b13f6843a
DaraTrachuk <144225545+DaraTrachuk@users.noreply.github.com>
2023-12-30 13:10:16 +0200

    Add files via upload

--- lesson_21/js/script.js
+++ lesson_21/js/script.js
@@ -3,3 +3,13 @@ const icon = document.querySelector('.icon-menu');
 icon.addEventListener('click', function () {
 	document.documentElement.classList.toggle('menu-open');
 });
+
+window.onload = function () {
+	document.addEventListener('click', documentActions);
+	function documentActions(e) {
+		const targetElement = e.target;
+		if (targetElement.classList.contains('search-header__button')) {
+			document.querySelector('.search-header__form').classList.toggle('_active');
+		}
+	}
+}
