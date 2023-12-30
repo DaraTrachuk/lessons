04322f96de4901e5befdddfe243b492ac0cef3b6
DaraTrachuk <144225545+DaraTrachuk@users.noreply.github.com>
2023-12-30 13:36:44 +0200

    Add files via upload

--- lesson_21/js/script.js
+++ lesson_21/js/script.js
@@ -8,7 +8,7 @@ window.onload = function () {
 	document.addEventListener('click', documentActions);
 	function documentActions(e) {
 		const targetElement = e.target;
-		if (targetElement.classList.contains('search-header__button')) {
+		if (targetElement.classList.contains('search-header__img-button')) {
 			document.querySelector('.search-header__form').classList.toggle('_active');
 		}
 	}
